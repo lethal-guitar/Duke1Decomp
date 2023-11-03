ce15d77fc8561bfa0159047e6bca64891dd3a7ed
Nikolai Wuttke <lethal_guitar128@web.de>
2023-11-03 13:54:56 +0100

    Update license header

--- src/GAME.C
+++ src/GAME.C
@@ -1,7 +1,26 @@
 /* Copyright (C) 2023 Nikolai Wuttke-Hohendorf. All rights reserved.
  *
- * This code is licensed under the GPLv3 license.
- * See the LICENSE file at the root of this repository.
+ * This project is based on disassembly of executable files from the game
+ * Duke Nukem, Copyright (C) 1991 Apogee Software, Ltd.
+ *
+ * Some parts of the code are based on or have been adapted from the Cosmore
+ * project, Copyright (c) 2020-2022 Scott Smitelli.
+ * See LICENSE_Cosmore file at the root of the repository, or refer to
+ * https://github.com/smitelli/cosmore/blob/master/LICENSE.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
 #include <dos.h>
